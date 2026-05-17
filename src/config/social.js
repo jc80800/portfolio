e5a0d7eb0f221e6e11dbf17f4d7888001fcace35
Jason Chen <chenjason112@gmail.com>
2026-05-17 16:40:02 -0400

    update social links

--- src/config/social.js
+++ src/config/social.js
@@ -1,5 +1,5 @@
 export const SOCIAL = {
-  github: 'https://github.com/YOUR_USERNAME',
-  linkedin: 'https://www.linkedin.com/in/YOUR_PROFILE',
-  email: 'mailto:you@example.com',
+  github: 'https://github.com/jc80800',
+  linkedin: 'https://www.linkedin.com/in/jchen80/',
+  email: 'mailto:chenjason910@gmail.com',
 }
